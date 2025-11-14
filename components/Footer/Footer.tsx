f5355c514e543ec9d9930984969bd3ab227261a4
Oleh_Novosad <onovosad321@gmail.com>
2025-11-14 16:09:20 +0100

    comm

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: Yevhenii Bilotserkivskyi</p>
+          <p>Developer: Oleh Novosad</p>
           <p>
             Contact us:
             <a href="mailto:student@notehub.app"> student@notehub.app</a>
